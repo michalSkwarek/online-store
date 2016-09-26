e3acb82232fdee7b2a12d36be55e27f2a852e7ba
Michal <skwarek.michal.adrian@gmail.com>
2016-09-26 22:08:35 +0200

    Initial commit

---
+++ online-store/src/main/java/com/skwarek/onlineStore/domain/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.skwarek.onlineStore.domain;
+
+/**
+ * Created by Michal on 26.09.2016.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Helo world!");
+    }
+}
