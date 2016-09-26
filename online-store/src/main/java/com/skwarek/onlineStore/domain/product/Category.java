1ddf17a3cc6f223a610389139f7ff03c844bf9be
Michal <skwarek.michal.adrian@gmail.com>
2016-09-27 00:21:38 +0200

    Create category model

---
+++ online-store/src/main/java/com/skwarek/onlineStore/domain/product/Category.java
@@ -0,0 +1,53 @@
+package com.skwarek.onlineStore.domain.product;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Michal on 27.09.2016.
+ */
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private Set<Product> products;
+
+    public Category() { }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
