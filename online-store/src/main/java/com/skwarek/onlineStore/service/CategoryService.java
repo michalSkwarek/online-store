63e2f148cb9109638c238b1b3a601ea138fd8e6c
Michal <skwarek.michal.adrian@gmail.com>
2016-09-27 00:26:37 +0200

    Create category service interface and implementation this interface

---
+++ online-store/src/main/java/com/skwarek/onlineStore/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.skwarek.onlineStore.service;
+
+import com.skwarek.onlineStore.domain.product.Category;
+import com.skwarek.onlineStore.service.generic.GenericService;
+
+/**
+ * Created by Michal on 27.09.2016.
+ */
+public interface CategoryService extends GenericService<Category, Long> {
+
+}
