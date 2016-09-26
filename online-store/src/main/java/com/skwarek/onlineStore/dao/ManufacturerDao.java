a059af490f201bd66700a496a8db4c11391f8417
Michal <skwarek.michal.adrian@gmail.com>
2016-09-27 01:24:11 +0200

    Create manufacturer dao interface and implementation this interface

---
+++ online-store/src/main/java/com/skwarek/onlineStore/dao/ManufacturerDao.java
@@ -0,0 +1,11 @@
+package com.skwarek.onlineStore.dao;
+
+import com.skwarek.onlineStore.dao.generic.GenericDao;
+import com.skwarek.onlineStore.domain.product.Manufacturer;
+
+/**
+ * Created by Michal on 27.09.2016.
+ */
+public interface ManufacturerDao extends GenericDao<Manufacturer, Long> {
+
+}
