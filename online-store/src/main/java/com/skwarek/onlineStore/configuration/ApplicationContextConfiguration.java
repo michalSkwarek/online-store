f5afdd0ef1cf6f9f63733103bff32cf3092ce732
Michal <skwarek.michal.adrian@gmail.com>
2016-10-30 22:21:51 +0100

    Added Apache Tiles annotations config

--- online-store/src/main/java/com/skwarek/onlineStore/configuration/ApplicationContextConfiguration.java
+++ online-store/src/main/java/com/skwarek/onlineStore/configuration/ApplicationContextConfiguration.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 /**
  * Created by Michal on 16.09.2016.
@@ -15,7 +19,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.skwarek.onlineStore" })
-public class ApplicationContextConfiguration {
+public class ApplicationContextConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public ViewResolver viewResolver() {
@@ -33,5 +37,31 @@ public class ApplicationContextConfiguration {
 //        multipartResolver.setMaxUploadSize(20971520); // 20MB
 //        multipartResolver.setMaxInMemorySize(1048576);	// 1MB
 //        return multipartResolver;
+//    }
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions("/WEB-INF/tiles/tiles.xml");
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
+
+    /**
+     * Configure ViewResolvers to deliver preferred views.
+     */
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        TilesViewResolver viewResolver = new TilesViewResolver();
+        registry.viewResolver(viewResolver);
+    }
+
+//    /**
+//     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+//     */
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
 //    }
 }
