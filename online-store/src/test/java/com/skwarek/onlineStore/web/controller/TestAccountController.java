395a3d35f829948e16ec3b133df4a8a0407ac39d
Michal <skwarek.michal.adrian@gmail.com>
2016-11-30 20:25:13 +0100

    Updated controller tests

--- online-store/src/test/java/com/skwarek/onlineStore/web/controller/TestAccountController.java
+++ online-store/src/test/java/com/skwarek/onlineStore/web/controller/TestAccountController.java
@@ -1,32 +1,23 @@
 package com.skwarek.onlineStore.web.controller;
 
 import com.skwarek.onlineStore.configuration.ApplicationContextConfiguration;
-import com.skwarek.onlineStore.data.dao.AccountDao;
 import com.skwarek.onlineStore.data.entity.user.Account;
 import com.skwarek.onlineStore.service.AccountService;
 import com.skwarek.onlineStore.web.validator.UsernameValidator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Date;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -35,40 +26,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Created by Michal on 29/11/2016.
  */
 @RunWith(MockitoJUnitRunner.class)
-@ContextConfiguration(classes = ApplicationContextConfiguration.class)
-@WebAppConfiguration
-@Transactional
 public class TestAccountController {
 
-    @Mock
     private AccountService accountService;
-
-    @Mock
     private UsernameValidator usernameValidator;
-
-    @InjectMocks
     private AccountController accountController;
 
     private MockMvc mockMvc;
 
+    private String username;
     private Account newAccount;
 
     @Before
     public void setUp() {
-        newAccount = new Account();
-        newAccount.setId(1L);
-        newAccount.setUsername("user");
-        newAccount.setPassword("pass");
-        newAccount.setEnabled(true);
-        newAccount.setEmail("email@gmail.com");
-        newAccount.setDateCreated(new Date());
-        newAccount.setRole(Account.ROLE_USER);
+        this.username = "user";
+
+        this.newAccount = new Account();
+        this.newAccount.setId(1L);
+        this.newAccount.setUsername(username);
+        this.newAccount.setPassword("pass");
+        this.newAccount.setEnabled(true);
+        this.newAccount.setEmail("email@gmail.com");
+        this.newAccount.setDateCreated(new Date());
+        this.newAccount.setRole(Account.ROLE_USER);
+
+        this.accountService = mock(AccountService.class);
+        this.usernameValidator = mock(UsernameValidator.class);
+        this.accountController = new AccountController(accountService, usernameValidator);
 
         this.mockMvc = MockMvcBuilders.standaloneSetup(this.accountController)
                 .build();
 
-        MockitoAnnotations.initMocks(this);
-        given(this.accountService.findAccountByUsername(newAccount.getUsername())).willReturn(newAccount);
+        given(this.accountService.findAccountByUsername(username)).willReturn(newAccount);
     }
 
     @Test
@@ -107,32 +96,28 @@ public class TestAccountController {
 
     @Test
     public void testInitUpdateAccountForm() throws Exception {
-        given(this.accountService.findAccountByUsername(newAccount.getUsername())).willReturn(newAccount);
-        System.out.println("qqq " + this.accountService.findAccountByUsername(newAccount.getUsername()));
-
-        mockMvc.perform(get("/accounts/edit/{username}", newAccount.getUsername()))
+        mockMvc.perform(get("/accounts/edit/{username}", username))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("account"))
-//                .andExpect(model().attribute("account", hasProperty("username", is("user"))))
-//                .andExpect(model().attribute("account", hasProperty("password", is("pass"))))
-//                .andExpect(model().attribute("account", hasProperty("email", is("email@gmail.com"))))
+                .andExpect(model().attribute("account", newAccount))
                 .andExpect(view().name("accounts/accountData"));
+
+        verify(accountService, times(1)).findAccountByUsername(username);
     }
 
     @Test
     public void testProcessUpdateAccountFormSuccess() throws Exception {
-        mockMvc.perform(post("/accounts/edit/{username}", newAccount.getUsername())
+        mockMvc.perform(post("/accounts/edit/{username}", username)
                 .param("password", "pass1")
                 .param("email", "email1@gmail.com")
         )
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/users/{username}"));
+                .andExpect(view().name("redirect:/users/" + username));
     }
 
     @Test
     public void testProcessUpdateAccountFormHasErrors() throws Exception {
-        accountService.createAccount(newAccount);
-        mockMvc.perform(post("/accounts/edit/{username}", newAccount.getUsername())
+        mockMvc.perform(post("/accounts/edit/{username}", username)
                 .param("password", "password")
                 .param("email", "emailgmail.com")
         )
--- online-store/src/test/java/com/skwarek/onlineStore/web/controller/TestCustomerController.java
+++ online-store/src/test/java/com/skwarek/onlineStore/web/controller/TestCustomerController.java
@@ -1,142 +1,145 @@
-//package com.skwarek.onlineStore.web.controller;
-//
-//import com.skwarek.onlineStore.configuration.ApplicationContextConfiguration;
-//import com.skwarek.onlineStore.data.dao.AccountDao;
-//import com.skwarek.onlineStore.data.entity.user.Account;
-//import com.skwarek.onlineStore.data.entity.user.Customer;
-//import com.skwarek.onlineStore.service.AccountService;
-//import com.skwarek.onlineStore.service.CustomerService;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//import static org.mockito.BDDMockito.given;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-///**
-// * Created by Michal on 29/11/2016.
-// */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = ApplicationContextConfiguration.class)
-//@WebAppConfiguration
-////@Transactional
-//public class TestCustomerController {
-//
-//    private static final long ID = 1;
-//
-//    @Autowired
-//    private WebApplicationContext wac;
-//
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private CustomerService customerService;
-//
-//    @Mock
-//    private AccountDao accountDao;
-//
-//    private Customer newCustomer;
-//
-//    @Before
-//    public void setUp() {
-//        newCustomer = new Customer();
-//        newCustomer.setFirstName("John");
-//        newCustomer.setLastName("Doe");
-//        newCustomer.setBirthDate("2000-06-16");
-//        newCustomer.setPhoneNumber("123456789");
-//        newCustomer.setNumberOfOrders(1);
-//
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
-//                .build();
-//        accountService.createAccount(newAccount);
-//        System.out.println("dupa " + this.accountService.findAccountByUsername("user"));
-//        given(this.accountDao.findAccountByUsername("user")).willReturn(newAccount);
-//    }
-//
-//    @Test
-//    public void testInitCreateCustomerForm() throws Exception {
-//        mockMvc.perform(get("/customers/new"))
-//                .andExpect(status().isOk())
-//                .andExpect(model().attributeExists("customer"))
-//                .andExpect(view().name("customers/customerData"));
-//    }
-//
-//    @Test
-//    public void testProcessCreateCustomerFormSuccess() throws Exception {
-//        mockMvc.perform(post("/customers/new")
-//                .param("firstName", "John")
-//                .param("lastName", "Doe")
-//                .param("birthDate", "2000-06-16")
-//                .param("phoneNumber", "123456789")
-//        )
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(view().name("redirect:/addresses/new"));
-//    }
-//
-//    @Test
-//    public void testProcessCreateCustomerFormHasErrors() throws Exception {
-//        mockMvc.perform(post("/customers/new")
-//                .param("firstName", "")
-//                .param("lastName", "")
-//                .param("birthDate", "2000-VI-16")
-//                .param("phoneNumber", "123456789")
-//        )
-//                .andExpect(status().isOk())
-//                .andExpect(model().attributeHasErrors("customer"))
-//                .andExpect(model().attributeHasFieldErrors("customer", "firstName"))
-//                .andExpect(model().attributeHasFieldErrors("customer", "lastName"))
-//                .andExpect(model().attributeHasFieldErrors("customer", "birthDate"))
-//                .andExpect(view().name("customers/customerData"));
-//    }
-//
-//    @Test
-//    public void testInitUpdateCustomerForm() throws Exception {
-//        mockMvc.perform(get("/customers/edit/{username}", newAccount.getUsername()))
-//                .andExpect(status().isOk())
-//                .andExpect(model().attributeExists("account"))
-//                .andExpect(model().attribute("account", hasProperty("username", is("user"))))
-//                .andExpect(model().attribute("account", hasProperty("password", is("pass"))))
-//                .andExpect(model().attribute("account", hasProperty("email", is("email@gmail.com"))))
-//                .andExpect(view().name("accounts/accountData"));
-//    }
-//
-//    @Test
-//    public void testProcessUpdateCustomerFormSuccess() throws Exception {
-////        accountService.createAccount(newAccount);
-//        mockMvc.perform(post("/customers/edit/{username}", newCustomer.getUsername())
-//                .param("firstName", "John1")
-//                .param("lastName", "Doe1")
-//                .param("birthDate", "2000-06-17")
-//                .param("phoneNumber", "111222333")
-//        )
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(view().name("redirect:/users/{username}"));
-//    }
-//
-//    @Test
-//    public void testProcessUpdateCustomerFormHasErrors() throws Exception {
-//        accountService.createAccount(newAccount);
-//        mockMvc.perform(post("/customers/edit/{username}", newCustomer.getUsername())
-//                .param("firstName", "")
-//                .param("lastName", "")
-//                .param("birthDate", "2000-VI-17")
-//                .param("phoneNumber", "111222333")
-//        )
-//                .andExpect(status().isOk())
-//                .andExpect(model().attributeHasErrors("customer"))
-//                .andExpect(model().attributeHasFieldErrors("customer", "firstName"))
-//                .andExpect(model().attributeHasFieldErrors("customer", "lastName"))
-//                .andExpect(model().attributeHasFieldErrors("customer", "birthDate"))
-//                .andExpect(view().name("customers/customerData"));
-//    }
-//}
+package com.skwarek.onlineStore.web.controller;
+
+import com.skwarek.onlineStore.configuration.ApplicationContextConfiguration;
+import com.skwarek.onlineStore.data.dao.AccountDao;
+import com.skwarek.onlineStore.data.entity.user.Account;
+import com.skwarek.onlineStore.data.entity.user.Customer;
+import com.skwarek.onlineStore.service.AccountService;
+import com.skwarek.onlineStore.service.CustomerService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Created by Michal on 29/11/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestCustomerController {
+
+    private AccountService accountService;
+    private CustomerService customerService;
+    private CustomerController customerController;
+
+    private MockMvc mockMvc;
+
+    private String username;
+    private Customer newCustomer;
+    private Account newAccount;
+
+    @Before
+    public void setUp() {
+        this.username = "user";
+
+        this.newCustomer = new Customer();
+        this.newCustomer.setId(1L);
+        this.newCustomer.setFirstName("John");
+        this.newCustomer.setLastName("Doe");
+        this.newCustomer.setBirthDate("2000-06-16");
+        this.newCustomer.setPhoneNumber("123456789");
+        this.newCustomer.setNumberOfOrders(1);
+
+        this.newAccount = new Account();
+        this.newAccount.setCustomer(newCustomer);
+
+        this.accountService = mock(AccountService.class);
+        this.customerService = mock(CustomerService.class);
+        this.customerController = new CustomerController(accountService, customerService);
+
+        this.mockMvc = MockMvcBuilders.standaloneSetup(this.customerController)
+                .build();
+
+        given(this.accountService.findAccountByUsername(username)).willReturn(newAccount);
+    }
+
+    @Test
+    public void testInitCreateCustomerForm() throws Exception {
+        mockMvc.perform(get("/customers/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("customer"))
+                .andExpect(view().name("customers/customerData"));
+    }
+
+    @Test
+    public void testProcessCreateCustomerFormSuccess() throws Exception {
+        mockMvc.perform(post("/customers/new")
+                .param("firstName", "John")
+                .param("lastName", "Doe")
+                .param("birthDate", "2000-06-16")
+                .param("phoneNumber", "123456789")
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/addresses/new"));
+    }
+
+    @Test
+    public void testProcessCreateCustomerFormHasErrors() throws Exception {
+        mockMvc.perform(post("/customers/new")
+                .param("firstName", "")
+                .param("lastName", "")
+                .param("birthDate", "2000-VI-16")
+                .param("phoneNumber", "123456789")
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasErrors("customer"))
+                .andExpect(model().attributeHasFieldErrors("customer", "firstName"))
+                .andExpect(model().attributeHasFieldErrors("customer", "lastName"))
+                .andExpect(model().attributeHasFieldErrors("customer", "birthDate"))
+                .andExpect(view().name("customers/customerData"));
+    }
+
+    @Test
+    public void testInitUpdateCustomerForm() throws Exception {
+        mockMvc.perform(get("/customers/edit/{username}", username))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("customer"))
+                .andExpect(model().attribute("customer", newCustomer))
+                .andExpect(view().name("customers/customerData"));
+
+        verify(accountService, times(1)).findAccountByUsername(username);
+    }
+
+    @Test
+    public void testProcessUpdateCustomerFormSuccess() throws Exception {
+        mockMvc.perform(post("/customers/edit/{username}", username)
+                .param("firstName", "John1")
+                .param("lastName", "Doe1")
+                .param("birthDate", "2000-06-17")
+                .param("phoneNumber", "111222333")
+        )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/users/" + username));
+    }
+
+    @Test
+    public void testProcessUpdateCustomerFormHasErrors() throws Exception {
+        mockMvc.perform(post("/customers/edit/{username}", username)
+                .param("firstName", "")
+                .param("lastName", "")
+                .param("birthDate", "2000-VI-17")
+                .param("phoneNumber", "111222333")
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasErrors("customer"))
+                .andExpect(model().attributeHasFieldErrors("customer", "firstName"))
+                .andExpect(model().attributeHasFieldErrors("customer", "lastName"))
+                .andExpect(model().attributeHasFieldErrors("customer", "birthDate"))
+                .andExpect(view().name("customers/customerData"));
+    }
+}
